ac0f657f4b1d8038ea56b4f70a007c1d344942d2
MickenCZ <MickenCZ@seznam.cz>
2023-12-25 11:07:11 +0100

    made background more consistent

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ else {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Váš Receptář</title>
     <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
     <?php 
